6c7ff41b5324adab3251bddd4a288956aa32764d
nerv <nervgh@gmail.com>
2015-07-28 00:06:29 +0300

    Updated: gulp & webpack instead Grunt, Coffee, etc.

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+
+
+var pkg = require('./package.json');
+// https://github.com/gulpjs/gulp/blob/master/docs/README.md
+var gulp = require('gulp');
+// http://webpack.github.io/docs/
+var webpack = require('webpack');
+// https://github.com/shama/webpack-stream
+var webpackStream = require('webpack-stream');
+
+
+gulp.task(
+    pkg.name + '/build',
+    function() {
+        return gulp
+            .src('./src/module.js')
+            .pipe(webpackStream({
+                plugins: [
+                    // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+                    new webpack.optimize.UglifyJsPlugin({
+                        compress: {
+                            warnings: false
+                        }
+                    }),
+                    // http://webpack.github.io/docs/list-of-plugins.html#bannerplugin
+                    new webpack.BannerPlugin(
+                        '/*\n' +
+                        ' ' + pkg.name + ' v' + pkg.version + '\n' +
+                        ' ' + pkg.homepage + '\n' +
+                        '*/\n'
+                    , {
+                        entryOnly: true,
+                        raw: true
+                    })
+                ],
+                devtool: 'source-map',
+                debug: true,
+                output: {
+                    library: pkg.name,
+                    libraryTarget: 'umd',
+                    filename: pkg.name + '.min.js'
+                }
+            }))
+            .pipe(gulp.dest('./dist'));
+    }
+);
+
+
+gulp.task(
+    pkg.name + '/watch', function() {
+        return gulp
+            .watch(
+                [
+                    './src/**/*.js',
+                    './src/**/*.json',
+                    './src/**/*.html'
+                ],
+                [
+                    pkg.name + '/build'
+                ]
+        );
+    }
+);
