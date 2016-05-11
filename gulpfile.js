85f8373f8aef8d1e979ad0c86efa6f8b31efb624
nerv <nervgh@gmail.com>
2016-05-11 11:47:16 +0300

    Added: a constraint of Node.js version >=4.0.0 Updated: devDependencies in the package.json; simplified the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,35 @@
-var pkg = require('./package.json');
+'use strict';
+
 // https://github.com/gulpjs/gulp/blob/master/docs/README.md
-var gulp = require('gulp');
-// http://webpack.github.io/docs/
-var webpack = require('webpack');
+let gulp = require('gulp');
 // https://github.com/shama/webpack-stream
-var webpackStream = require('webpack-stream');
-// https://github.com/dominictarr/event-stream
-var es = require('event-stream');
-// https://github.com/justmoon/node-extend
-var extend = require('extend');
+let webpackStream = require('webpack-stream');
+
+let WebpackConfig = require('./WebpackConfig');
+let descriptor = require('./package.json');
+
+
+let config = new WebpackConfig(descriptor, {
+  src: './src',
+  dist: './dist'
+});
+
 
 gulp.task(
-    pkg.name + '/build',
-    function() {
-        var normalWebpackStream = {
-                module: {
-                    loaders: [
-                        // https://github.com/babel/babel-loader
-                        {test: /\.js$/, loader: 'babel'},
-                        // https://github.com/webpack/json-loader
-                        {test: /\.json$/, loader: 'json'},
-                        // https://github.com/webpack/html-loader
-                        {test: /\.html$/, loader: 'html'}
-                    ]
-                },
-                plugins: [
-
-                    // http://webpack.github.io/docs/list-of-plugins.html#bannerplugin
-                    new webpack.BannerPlugin(
-                        '/*\n' +
-                        ' ' + pkg.name + ' v' + pkg.version + '\n' +
-                        ' ' + pkg.homepage + '\n' +
-                        '*/\n'
-                    , {
-                        entryOnly: true,
-                        raw: true
-                    })
-                ],
-                devtool: 'source-map',
-                debug: true,
-                output: {
-                    library: pkg.name,
-                    libraryTarget: 'umd',
-                    filename: pkg.name + '.js'
-                }
-            },
-            normalStream =
-                gulp
-                    .src('./src/index.js')
-                    .pipe(webpackStream(normalWebpackStream))
-                    .pipe(gulp.dest('./dist'));
-
-        /**
-         * Deep copy the normalWebpackStream to customize it for the uglify stream
-         */
-        var ulgifyWebpackStream = extend(true, {}, normalWebpackStream);
-
-        ulgifyWebpackStream.plugins.unshift(
-            // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-            new webpack.optimize.UglifyJsPlugin({
-                compress: {
-                    warnings: false
-                }
-        }));
-
-        ulgifyWebpackStream.output.filename = pkg.name + '.min.js';
-
-        var uglifyStream =
-            gulp
-                .src('./src/index.js')
-                .pipe(webpackStream(ulgifyWebpackStream))
-                .pipe(gulp.dest('./dist'));
-
-        return es.concat(normalStream, uglifyStream);
-    }
+  `${config.name}/build`,
+  function () {
+    return gulp
+      .src(config.path.src)
+      .pipe(webpackStream(config.get()))
+      .pipe(gulp.dest(config.path.dist));
+  }
 );
 
-
 gulp.task(
-    pkg.name + '/watch', function() {
-        return gulp
-            .watch(
-                [
-                    './src/**/*.js',
-                    './src/**/*.json',
-                    './src/**/*.html'
-                ],
-                [
-                    pkg.name + '/build'
-                ]
-        );
-    }
+  `${config.name}/watch`, function () {
+    return gulp
+      .watch(`${config.path.src}**/*.*`, [
+        `${config.name}/build`
+      ]);
+  }
 );
