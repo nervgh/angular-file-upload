dc92cb7ac3d72fe0a732b4ff427e4f5e5b5abb01
yuangongji <yuangongji@foxmail.com>
2019-12-10 16:05:25 +0800

    issue-862 solution

---
+++ examples/issues/862/controllers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+angular
+
+
+    .module('app', ['angularFileUpload'])
+
+
+    .controller('AppController', ['$scope', 'FileUploader', function($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.php'
+            //,timeout: 2000
+        });
+
+        $scope.testUploader = function(){
+            angular.element("#testUpload").click();
+        }        
+
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+    }]);
